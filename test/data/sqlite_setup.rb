2969885f8275405692f9da0f5c5f34be806edde0
E-Max <max@studentify.nl>
2012-05-04 00:10:02 +0200

    Revert "sqlite db setup"
    This reverts commit ab672a24b9632045c9de7fb178565970fb08c8a4.

--- test/data/sqlite_setup.rb
+++
@@ -1,9 +0,0 @@
-require 'sqlite3'
-
-class SqliteSetup
-  TEST_DATABASE_PATH = File.expand_path("#{File.dirname(__FILE__)}/../temp/test.db").freeze
-
-  def self.create_db
-    SQLite3::Database.new TEST_DATABASE_PATH
-  end
-end
